c2827cacfb7e3618790bca14bb4fc66b9f333a7b
Денис Голубев (Работа) <amigo36@yandex.ru>
2020-12-09 16:21:13 +0300

    Добавьте файлы проекта.

---
+++ Lesson-1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите свое имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Добрый день, {name}. Сегодня - {DateTime.Now.ToShortDateString()}");
+            Console.ReadKey();
+        }
+    }
+}
