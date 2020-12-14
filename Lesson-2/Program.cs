c9d8cf573549c446ff1540bc122de3fdaa0e9b30
Денис Голубев <amigo36@yandex.ru>
2020-12-15 01:46:06 +0300

    Пока исправил Упражнение 1, использую goto, а это плохо. Не понимаю как обойти.

--- Lesson-2/Program.cs
+++ Lesson-2/Program.cs
@@ -11,33 +11,32 @@ namespace Lesson_2
         static void Main(string[] args)
         {
             Console.WriteLine("Упражнение 1\n");
-            Min_Max_Temp aver_Temp = new Min_Max_Temp();
-            aver_Temp.average_Temp();
-            double c = aver_Temp.aver_Temp();
-            Console.WriteLine("\nСреднесуточная температура: " + c + " градусов цельсия");
+            MinMaxTemp aver_Temp = new MinMaxTemp();
+            aver_Temp.EnterTemp();
+            aver_Temp.AveregeTemp();
             Console.WriteLine(new string('-', 50));
             //
             //-----------------------------------------------------------------------------------
             //
-            Console.WriteLine("\nУпражнение 2\n");
-            SerialNumberMonth month = new SerialNumberMonth();
-            string m = month.Number_of_month();
-            Console.WriteLine("\nВы выбрали: " + m);
-            Console.WriteLine(new string('-', 50));
-            //
-            //-----------------------------------------------------------------------------------
-            //
-            Console.WriteLine("\nУпражнение 3\n");
-            Even_number number = new Even_number();
-            if (number.even_number() == true) Console.WriteLine("Вы ввели четное число");
-            else Console.WriteLine("Вы ввели нечетное число");
-            Console.WriteLine(new string('-', 50));
-            //
-            //-----------------------------------------------------------------------------------
-            //
-            Console.WriteLine("\nУпражнение 4\n");
-            if ((m == "Декабрь" || m == "Январь" || m == "Февраль") && c > 0) Console.WriteLine("Дождливая зима");
-            else Console.WriteLine("С зимой все впорядке");
+            //Console.WriteLine("\nУпражнение 2\n");
+            //SerialNumberMonth month = new SerialNumberMonth();
+            //string m = month.Number_of_month();
+            //Console.WriteLine("\nВы выбрали: " + m);
+            //Console.WriteLine(new string('-', 50));
+            ////
+            ////-----------------------------------------------------------------------------------
+            ////
+            //Console.WriteLine("\nУпражнение 3\n");
+            //Even_number number = new Even_number();
+            //if (number.even_number() == true) Console.WriteLine("Вы ввели четное число");
+            //else Console.WriteLine("Вы ввели нечетное число");
+            //Console.WriteLine(new string('-', 50));
+            ////
+            ////-----------------------------------------------------------------------------------
+            ////
+            //Console.WriteLine("\nУпражнение 4\n");
+            //if ((m == "Декабрь" || m == "Январь" || m == "Февраль") && c > 0) Console.WriteLine("Дождливая зима");
+            //else Console.WriteLine("С зимой все впорядке");
 
             Console.ReadKey();
 
