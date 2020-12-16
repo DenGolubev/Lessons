f2be13233c0d42552c52f1df33aec3f041140478
Денис Голубев (Работа) <amigo36@yandex.ru>
2020-12-16 16:58:48 +0300

    Упражнение 1

---
+++ Lesson-3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            TwoDimensionalArray twoArray = new TwoDimensionalArray();
+            twoArray.OutputElements();
+        }
+    }
+}
---
+++ Lesson-3/TwoDimensionalArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_3
+{
+    class TwoDimensionalArray
+    {
+        private int[,] PhoneDirectory = new int[5,5];
+
+        private void InputElements()
+        {
+            Random variableRND = new Random();
+           
+
+            for (int i = 0; i < PhoneDirectory.GetLength(0); i++)
+            {
+                for (int y = 0; y < PhoneDirectory.GetLength(1); y++)
+                {
+                    PhoneDirectory[i, y] = variableRND.Next(10);
+                }
+                
+            }
+        }
+        public void OutputElements()
+        {
+            InputElements();
+            for (int i = 0; i < PhoneDirectory.GetLength(0); i++)
+            {
+                for (int y = 0; y < PhoneDirectory.GetLength(1); y++)
+                {
+                    if (i == y)
+                    {
+                        Console.Write("{0}\t",PhoneDirectory[i, y]);
+                    }
+                    
+                }
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
