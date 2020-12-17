37d05cb5a7ae54867db3952359bf66555c437b41
Денис Голубев (Работа) <amigo36@yandex.ru>
2020-12-17 17:05:28 +0300

    Добавил меню

--- Lesson-3/PhoneDirectory.cs
+++ Lesson-3/PhoneDirectory.cs
@@ -8,6 +8,28 @@ namespace Lesson_3
         string[,] PhoneBook = new string[5, 2];
         string[] NameBook = new string[5];
         string[] NumberBook = new string[5];
+
+        public void MyMenu()
+        {
+            Console.WriteLine("Выберите пункт меню:");
+            Console.WriteLine("Добавить контакт - 1\nПосмотреть контакты - 2\nВыйти из приложения - 3");
+            int i = int.Parse(Console.ReadLine());
+            switch (i)
+            {
+                case 1:
+                    AddUser();
+                    break;
+                case 2:
+                    OutUser();
+                    break;
+                case 3:
+                    Environment.Exit(3);
+                    break;
+                default:
+                    Console.WriteLine("Вы ввели не правильную цифру");
+                    break;
+            }
+        }
         public void AddUser()
         {
             Console.WriteLine("Введите имя контакта:");
