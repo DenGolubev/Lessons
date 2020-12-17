7505723ced358401a35e17f49eaf1e84070033a0
Денис Голубев (Работа) <amigo36@yandex.ru>
2020-12-17 16:51:01 +0300

    Выполняю Упражнение 2

--- Lesson-3/PhoneDirectory.cs
+++ Lesson-3/PhoneDirectory.cs
@@ -6,14 +6,22 @@ namespace Lesson_3
     class PhoneDirectory
     {
         string[,] PhoneBook = new string[5, 2];
-        ArrayList User = new ArrayList();
+        string[] NameBook = new string[5];
+        string[] NumberBook = new string[5];
         public void AddUser()
         {
             Console.WriteLine("Введите имя контакта:");
             string name = Console.ReadLine();
             Console.WriteLine("Введите номер телефона:");
             string phone = Console.ReadLine();
-            User.AddRange(new string[] { name, phone });
+            for (int y   = 0; y < PhoneBook.GetLength(0); y++)
+            {
+                NameBook[y] = name;
+                NumberBook[y] = phone;
+                Console.Write("Имя {0} / Телефон {1}", NameBook[y], NumberBook[y]);
+                Console.ReadKey();
+                break;
+            }     
             
         }
 
