c3cedd217bbc9b08564349f1d43e1bddf595a14c
Денис Голубев (Работа) <amigo36@yandex.ru>
2020-12-28 13:15:33 +0300

    Упражнение 6 - выполнено

---
+++ Lesson-5/SerialBin.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using System.IO;
+
+
+namespace Lesson_5
+{
+    
+    class SerialBin
+    {
+        FileMethod fileMethod = new FileMethod();
+        
+        public void BinFile()
+        {
+            string mytext = fileMethod.InputFile();
+        }
+    }
+}
