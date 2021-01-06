c9af18c71283f29b3d21e92e50ce3726e38515bd
Денис Голубев <amigo36@yandex.ru>
2021-01-06 22:45:43 +0300

    Добавил вывод приветствия в начало

--- Lesson-8/Program.cs
+++ Lesson-8/Program.cs
@@ -8,9 +8,10 @@ namespace Lesson_8
         static void Main(string[] args)
         {
             UserData user = new UserData();
-            
+            string greeting = Properties.Settings.Default.Greeting;
             if (string.IsNullOrEmpty(Properties.Settings.Default.UserName) || string.IsNullOrEmpty(Properties.Settings.Default.UserCareer))
             {
+                Console.WriteLine(greeting);
                 user.GetUserData();
                 Properties.Settings.Default.UserName = user.Name;
                 Properties.Settings.Default.UserCareer = user.Career;
@@ -20,8 +21,8 @@ namespace Lesson_8
             string name = Properties.Settings.Default.UserName;
             string career = Properties.Settings.Default.UserCareer;
             int age = Properties.Settings.Default.UserAge;
-            string greeting = Properties.Settings.Default.Greeting;
-            Console.WriteLine($"{greeting} {name}, Ваш возраст {age} и вы по профессии {career}");
+            
+            Console.WriteLine($"{greeting}\n {name}, Ваш возраст {age} и вы по профессии {career}");
             Console.ReadKey();
         }
     }
