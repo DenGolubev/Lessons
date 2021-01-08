d68510725ae7f8d842b37539b4131c3f1bcd2e8c
Денис Голубев <amigo36@yandex.ru>
2021-01-08 19:16:04 +0300

    Использовал кортеж

--- Lesson-8/Program.cs
+++ Lesson-8/Program.cs
@@ -10,30 +10,42 @@ namespace Lesson_8
         static void Main(string[] args)
         {
             string greeting = Properties.Settings.Default.Greeting;
-            if (string.IsNullOrEmpty(Properties.Settings.Default.UserName) || string.IsNullOrEmpty(Properties.Settings.Default.UserCareer)) UserData(greeting);
+            Console.WriteLine(greeting);
+            if (string.IsNullOrEmpty(Properties.Settings.Default.UserName) || string.IsNullOrEmpty(Properties.Settings.Default.UserCareer))
+            {
+                var prop = UserData();
+                UserData newUser = new UserData(prop.Item1, prop.Item2, prop.Item3);
+                PrintUserData(newUser.Name, newUser.Career, newUser.Age);
+            }
             else
             {
                 UserData oldUser = new UserData(Properties.Settings.Default.UserName, Properties.Settings.Default.UserCareer, Properties.Settings.Default.UserAge);
-                PrintUserData(greeting, oldUser.Name, oldUser.Career, oldUser.Age);
-            } 
-                
+                PrintUserData(oldUser.Name, oldUser.Career, oldUser.Age);
+            }
+           
+
         }
 
-        public static void UserData(string greeting)
+        public static (string, string, int) UserData()
         {
-            Console.WriteLine(greeting);
             Console.WriteLine("Введите ваше имя");
             string name = Console.ReadLine();
             Console.WriteLine("Введите ваш род деятельности");
             string career = Console.ReadLine();
             Console.WriteLine("Введите ваш возраст");
             int age = int.Parse(Console.ReadLine());
-            UserData newUser = new UserData(name, career, age);
+            Properties.Settings.Default.UserName = name;
+            Properties.Settings.Default.UserCareer = career;
+            Properties.Settings.Default.UserAge = age;
+            Properties.Settings.Default.Save();
+            var RegUser = (name, career, age);
+            return RegUser;
         }
-        public static void PrintUserData(string greeting, string name, string career, int age)
+        public static void PrintUserData(string name, string career, int age)
         {
-            Console.WriteLine($"{greeting}\n {name}, Ваш возраст {age} и вы по профессии {career}");
+            Console.WriteLine($"\n{name}, Ваш возраст {age} и вы по профессии {career}");
             Console.ReadKey();
+            //Properties.Settings.Default.Reset();
         }
     }
 
